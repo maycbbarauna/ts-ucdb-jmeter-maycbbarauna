725a245e7a0236a23221f4821bebc1afd7e12f30
Leonides Fernando de Oliveira <fernando.oliveira@tecsinapse.com.br>
2018-06-15 23:19:58 -0400

    commit inicial

---
+++ src/main/java/br/ucdb/pos/engenhariasoftware/testesoftware/automacao/AutomacaoTesteSoftwareApplication.java
@@ -0,0 +1,12 @@
+package br.ucdb.pos.engenhariasoftware.testesoftware.automacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomacaoTesteSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomacaoTesteSoftwareApplication.class, args);
+	}
+}
