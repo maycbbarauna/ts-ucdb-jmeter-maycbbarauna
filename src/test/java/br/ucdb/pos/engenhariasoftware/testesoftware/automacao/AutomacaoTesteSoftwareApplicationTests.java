5a692767316bff7de577920f2d9a3a5cd1be6388
Leonides Fernando de Oliveira <fernando.oliveira@tecsinapse.com.br>
2018-06-15 23:20:24 -0400

    veniando classe do spring boot

---
+++ src/test/java/br/ucdb/pos/engenhariasoftware/testesoftware/automacao/AutomacaoTesteSoftwareApplicationTests.java
@@ -0,0 +1,16 @@
+package br.ucdb.pos.engenhariasoftware.testesoftware.automacao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AutomacaoTesteSoftwareApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
